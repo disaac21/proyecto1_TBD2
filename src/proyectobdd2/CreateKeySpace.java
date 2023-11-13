b89f03385444cc267c2a07242248beb3b00094a2
SerlioGiron <serlio.giron@unitec.edu>
2023-11-13 14:21:11 -0600

    serlio - trabajando en java

--- src/proyectobdd2/CreateKeySpace.java
+++ src/proyectobdd2/CreateKeySpace.java
@@ -3,14 +3,15 @@ package proyectobdd2;
 public class CreateKeySpace {
 
     public static void main(String[] args) {
+     
         try {
             DBConnector connector = new DBConnector();
             connector.connectdb("localhost", 9042);
 
-            final String createmovieKeySpace = "CREATE KEYSPACE IF NOT EXISTS movies_keypsace WITH "
-                    + "replication = {'class' : 'SimpleStrategy'.´replication_factor' :3}";
-
-            connector.getSession().execute(createmovieKeySpace);
+//            final String createmovieKeySpace = "CREATE KEYSPACE IF NOT EXISTS movies_keypsace WITH "
+//                    + "replication = {'class' : 'SimpleStrategy'.´replication_factor' :3}";
+//
+//            connector.getSession().execute(createmovieKeySpace);
             
         } catch (Exception e) {
             e.printStackTrace();
