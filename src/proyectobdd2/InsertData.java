e6fcff7a6c84cf21ee4270f21ff0a7b3d4ac454f
SerlioGiron <serlio.giron@unitec.edu>
2023-11-13 17:10:39 -0600

    ya inserto

--- src/proyectobdd2/InsertData.java
+++ src/proyectobdd2/InsertData.java
@@ -1,32 +1,30 @@
 package proyectobdd2;
 
+import com.datastax.driver.core.BoundStatement;
 import com.datastax.driver.core.PreparedStatement;
 
 public class InsertData {
 
     public static void insertar() {
-     
+
         try {
             DBConnector connector = new DBConnector();
             connector.connectdb("localhost", 9042);
-            
-            String insertar = "INSERT INTO Licencia (tipo_lic) VALUES ('B');";
 
-//            final String createmovieKeySpace = "CREATE KEYSPACE IF NOT EXISTS movies_keypsace WITH "
-//                    + "replication = {'class' : 'SimpleStrategy'.´replication_factor' :3}";
-//
-            PreparedStatement psInsert = connector.getSession().prepare(insertar);
-    
-//            connector.getSession().execute(insertar);
+            String insertarQuery = "INSERT INTO autoescuela_keyspace.Licencia (tipo_lic) VALUES (?);";
+            PreparedStatement psInsert = connector.getSession().prepare(insertarQuery);
+            BoundStatement bsInsert = psInsert.bind("B");
+            
+            connector.getSession().execute(bsInsert);
+
             connector.close();
-            
-            
+
             System.out.println("SE PUDOOOOOOO!!!!!");
-            
+
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("algo malo paso a la hora de intentar insertar");
         }
-        
+
     }
 }
